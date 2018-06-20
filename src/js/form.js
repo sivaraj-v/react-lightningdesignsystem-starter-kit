1ebf1e48a0dc3e09c463656ab371b255d357f0db
BOSTON\sivaraj.v <zural143@gmail.com>
2018-06-20 13:29:33 +0530

    First Row Cols Updated

--- src/js/form.js
+++ src/js/form.js
@@ -9,55 +9,34 @@ import Button from "@salesforce/design-system-react/components/button";
 const accounts = [
   {
     id: "1",
-    label: "Acme",
+    label: "AY",
     subTitle: "Account • San Francisco",
-    type: "account",
+    type: "account"
   },
   {
     id: "2",
-    label: "Salesforce.com, Inc.",
+    label: "EXC",
     subTitle: "Account • San Francisco",
-    type: "account",
-  },
-  {
-    id: "3",
-    label: "Paddy's Pub",
-    subTitle: "Account • Boston, MA",
-    type: "account",
-  },
-  {
-    id: "4",
-    label: "Tyrell Corp",
-    subTitle: "Account • San Francisco, CA",
-    type: "account",
-  },
-  {
-    id: "5",
-    label: "Paper St. Soap Company",
-    subTitle: "Account • Beloit, WI",
-    type: "account",
-  },
-  {
-    id: "6",
-    label: "Nakatomi Investments",
-    subTitle: "Account • Chicago, IL",
-    type: "account",
-  },
-  { id: "7", label: "Acme Landscaping", type: "account" },
-  {
-    id: "8",
-    label: "Acme Construction",
-    subTitle: "Account • Grand Marais, MN",
-    type: "account",
-  },
+    type: "account"
+  }
 ];
+const currency = [
+  {
+    id: "1",
+    label: "Norway",
+    subTitle: "Account • San Francisco",
+    type: "account"
+  }
+];
+
+
 
 class form extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      inputValue: "",
-      selection: [],
+			program: '',
+			currency: ''
     };
     // this.handleButtonClicked = this.handleButtonClicked.bind(this);
   }
@@ -69,34 +48,85 @@ class form extends React.Component {
       console.log(this.state);
     }
     return (
-      <IconSettings iconPath="/css/icons">
-        <Combobox
-          id="combobox-unique-id"
-          events={{
-            onSelect: (event, data) => {
-              if (this.props.action) {
-                this.props.action("onSelect")(event, ...Object.keys(data).map(key => data[key]));
-              } else if (console) {
-                console.log("onSelect", event, data);
-              }
-              this.setState({
-                inputValue: "",
-                selection: data.selection,
-              });
-            },
-          }}
-          labels={{
-            label: "Search",
-            placeholder: "Search Salesforce",
-          }}
-          options={accounts}
-          selection={this.state.selection}
-          value={this.state.inputValue}
-          variant="readonly"
-        />
+      <div className="slds-grid slds-template_bottom-magnet">
+        <IconSettings iconPath="/css/icons">
+          <div className="slds-col slds-size_4-of-12">
+            <div className="slds-form-element">
+              <div className="slds-text-heading_small slds-p-around_xx-small">
+                React Ligthing demo
+              </div>
+              <div class="slds-col slds-grid slds-size_1-of-1">
+                <div class="slds-col slds-size_1-of-2 slds-p-around_xx-small">
+                  <Combobox
+                    id="combobox-unique-id"
+                    events={{
+                      onSelect: (event, data) => {
+                        if (this.props.action) {
+                          this.props.action("onSelect")(
+                            event,
+                            ...Object.keys(data).map(key => data[key])
+                          );
+                        } else if (console) {
+                          console.log("onSelect", event, data);
+                        }
+                        this.setState({
+                          ...this.state,
+                          program: data.selection[0].label
+                        });
+                      }
+                    }}
+                    labels={{
+                      label: "Program",
+                      placeholder: "Select program"
+                    }}
+                    options={accounts}
+                    selection={this.state.program}
+                    value={this.state.inputValue}
+                    variant="readonly"
+                    required
+                  />
+                </div>
+                <div class="slds-col slds-size_1-of-2 slds-p-around_xx-small">
+                  <Combobox
+                    id="combobox-unique-id"
+                    events={{
+                      onSelect: (event, data) => {
+                        if (this.props.action) {
+                          this.props.action("onSelect")(
+                            event,
+                            ...Object.keys(data).map(key => data[key])
+                          );
+                        } else if (console) {
+                          console.log("onSelect", event, data);
+                        }
+                        this.setState({
+                          ...this.state,
+                          currency: data.selection[0].label
+                        });
+                      }
+                    }}
+                    labels={{
+                      label: "Currency",
+                      placeholder: "Select currency"
+                    }}
+                    options={currency}
+                    selection={this.state.currency}
+                    value={this.state.inputValue}
+                    variant="readonly"
+                    required
+                  />
+                </div>
+              </div>
 
-        <Button id="tuesday-alt" label="Submit and go to next tab" onClick={e => this.handleButtonClicked(e)} />
-      </IconSettings>
+              <Button
+                id="tuesday-alt"
+                label="Submit and go to next tab"
+                onClick={e => this.handleButtonClicked(e)}
+              />
+            </div>
+          </div>
+        </IconSettings>
+      </div>
     );
   }
 }
